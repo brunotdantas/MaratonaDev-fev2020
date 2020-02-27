b8da45cf82313753368c442ce90d7d6345d6fd7d
Bruno Dantas <brunotdantas@outlook.com>
2020-02-27 00:12:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ nunjucks.configure('./',{
     const Pool = require('pg').Pool;
     const db = new Pool({
         user: process.env.DB_USER,
-        password:process.env.DB_PASS,
-        host:process.env.DB_HOST,
+        password: process.env.DB_PASS,
+        host: process.env.DB_HOST,
         port:  process.env.DB_PORT,
-        database:process.env.DB_DB_NAME,
+        database: process.env.DB_DB_NAME,
         ssl: true
     })
 
